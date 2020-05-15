2f5e3e9557fe37be72a884bbeb1f9f1fcd2d257c
HamdyTawfeek <hamdy.tawfeek92@gmail.com>
2020-05-16 00:22:34 +0200

    Added grouping data by one or more columns of date, channel, country, os

--- analysis/views.py
+++ analysis/views.py
@@ -2,6 +2,7 @@ from rest_framework.generics import ListAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter as DjangoOrderingFilter
 
+from django.db.models import Sum
 from .serializers import PerformanceSerializer
 from .models import Performance
 
@@ -17,4 +18,22 @@ class MetricList(ListAPIView):
         'os': ['exact']
     }
     ordering_fields = '__all__'
+
+
+    def get_queryset(self):
+        group_by = self.request.query_params.get('group_by', None)
+        if group_by is None:
+            return super().get_queryset()
+        
+        group_by = map(str.strip, self.request.query_params['group_by'].split(','))
+        queryset = Performance.objects.all()
+        grouped_queryset =  (queryset.values(*group_by)
+                            .annotate(
+                            clicks = Sum('clicks'),
+                            impressions = Sum('impressions'),
+                            installs = Sum('installs'),
+                            revenue = Sum('revenue'),
+                            spend = Sum('spend'))
+        )
+        return grouped_queryset
 
