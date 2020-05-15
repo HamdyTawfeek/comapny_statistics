07b9dc9bdf4a240576957cf356ed985d87c8fa28
HamdyTawfeek <hamdy.tawfeek92@gmail.com>
2020-05-16 00:16:04 +0200

    edit float fields to decimal fields for better represenation

--- analysis/models.py
+++ analysis/models.py
@@ -4,9 +4,6 @@ class Performance(models.Model):
     """
     Performance Metrics Data Model
     """
-    class Meta:
-        ordering = ['pk']
-
     date = models.DateField('Date')
     channel = models.CharField('Channel', max_length=50)
     country = models.CharField('Country', max_length=50)
@@ -14,5 +11,5 @@ class Performance(models.Model):
     impressions =  models.IntegerField('Impressions', default=0)
     clicks =  models.IntegerField('Clicks', default=0)
     installs =  models.IntegerField('Installs', default=0)
-    spend = models.FloatField('Spend', default=0) 
-    revenue = models.FloatField('Revenue', default=0) 
+    spend = models.DecimalField('Spend', max_digits=30, decimal_places=3, default=0) 
+    revenue = models.DecimalField('Revenue', max_digits=30, decimal_places=3, default=0) 
