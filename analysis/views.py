9cc4c1e6fc5a3a95359e2c6fc12da9042de7401c
HamdyTawfeek <hamdy.tawfeek92@gmail.com>
2020-05-15 19:55:32 +0200

    added metric view as a generic API endpoint

--- analysis/views.py
+++ analysis/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter as DjangoOrderingFilter
 
-# Create your views here.
+from .serializers import PerformanceSerializer
+from .models import Performance
+
+
+class MetricList(ListAPIView):
+    queryset = Performance.objects.all()
+    serializer_class = PerformanceSerializer
+    filter_backends = (DjangoFilterBackend, DjangoOrderingFilter,)
+    filterset_fields = {
+        'date': ['gte', 'lte', 'exact', 'gt', 'lt'],
+        'channel': ['exact'],
+        'country': ['exact'],
+        'os': ['exact']
+    }
+    ordering_fields = '__all__'
+
