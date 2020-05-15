7bbd0c618037022269887b94b9c1c60f530a1453
HamdyTawfeek <hamdy.tawfeek92@gmail.com>
2020-05-16 00:18:33 +0200

    validating serializers fields

--- analysis/serializers.py
+++ analysis/serializers.py
@@ -1,8 +1,24 @@
-from rest_framework import serializers
+from rest_framework.serializers import (ModelSerializer, DateField, 
+                                        CharField, IntegerField,
+                                        DecimalField)
 
 from .models import Performance
 
-class PerformanceSerializer(serializers.ModelSerializer):
+class PerformanceSerializer(ModelSerializer):
+    """
+    Performance Metrics Data Serializer
+    """
+    date = DateField(required=False, allow_null=False)
+    channel = CharField(required=False, allow_null=False)
+    country = CharField(required=False, allow_null=False)
+    os = CharField(required=False, allow_null=False)
+    impressions = IntegerField(required=False, allow_null=False)
+    clicks = IntegerField(required=False, allow_null=False)
+    installs = IntegerField(required=False, allow_null=False)
+    spend = DecimalField(required=False, max_digits=30, decimal_places=2, allow_null=False)
+    revenue = DecimalField(required=False, max_digits=30, decimal_places=2, allow_null=False)
+    
+
     class Meta:
         model = Performance
         fields = (
