e205d64627f22e8bc932256b85e2923b0ce33924
HamdyTawfeek <hamdy.tawfeek92@gmail.com>
2020-05-16 05:19:01 +0200

    organize metric endpoint logic

--- analysis/views.py
+++ analysis/views.py
@@ -2,7 +2,8 @@ from rest_framework.generics import ListAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter as DjangoOrderingFilter
 
-from django.db.models import Sum
+import operator
+from django.db.models import Sum, F, FloatField, ExpressionWrapper
 from .serializers import PerformanceSerializer
 from .models import Performance
 
@@ -15,25 +16,33 @@ class MetricList(ListAPIView):
         'date': ['gte', 'lte', 'exact', 'gt', 'lt'],
         'channel': ['exact'],
         'country': ['exact'],
-        'os': ['exact']
+        'os': ['exact'],
     }
     ordering_fields = '__all__'
-
+    
 
     def get_queryset(self):
-        group_by = self.request.query_params.get('group_by', None)
-        if group_by is None:
-            return super().get_queryset()
-        
-        group_by = map(str.strip, self.request.query_params['group_by'].split(','))
         queryset = Performance.objects.all()
-        queryset =  (queryset.values(*group_by)
-                            .annotate(
-                            clicks = Sum('clicks'),
-                            impressions = Sum('impressions'),
-                            installs = Sum('installs'),
-                            revenue = Sum('revenue'),
-                            spend = Sum('spend'))
-        )
+        group_by = self.request.query_params.get('group_by', None)
+        cpi = self.request.query_params.get('cpi', None)
+
+        if (group_by is None) and (cpi is None):
+            return super().get_queryset()
+
+        if group_by:
+            group_by = map(str.strip, self.request.query_params['group_by'].split(','))
+            queryset =  (Performance.objects.values(*group_by)
+                                .annotate(
+                                clicks = Sum('clicks'),
+                                impressions = Sum('impressions'),
+                                installs = Sum('installs'),
+                                revenue = Sum('revenue'),
+                                spend = Sum('spend'))
+            )
+            queryset = queryset.annotate(cpi=ExpressionWrapper(
+                    F('spend') / F('installs'),
+                    output_field=FloatField())
+            )
+                
         return queryset
 
